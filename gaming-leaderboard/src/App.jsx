4425ce3fecb18ebfc4ff86348ef07006978e9620
blovelace84 <blovelace84@gmail.com>
2025-01-16 19:58:54 -0500

    Remove search component and styles

--- gaming-leaderboard/src/App.jsx
+++ gaming-leaderboard/src/App.jsx
@@ -1,13 +1,11 @@
 import Header from "./Header";
 import Leaderboard from "./Leaderboard";
-import Search from "./Search";
 
 const App = () => {
     
     return(
         <div>
             <Header />
-            <Search />
             <Leaderboard />
         </div>
     );
