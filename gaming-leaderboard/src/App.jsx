794ffcb95d5e4f91a698d59adb51623a8ab302d4
blovelace84 <blovelace84@gmail.com>
2025-01-17 12:55:33 -0500

    Update changes to app component with styles

--- gaming-leaderboard/src/App.jsx
+++ gaming-leaderboard/src/App.jsx
@@ -1,12 +1,20 @@
 import Header from "./Header";
 import Leaderboard from "./Leaderboard";
+import './App.css';
 
 const App = () => {
     
     return(
-        <div>
-            <Header />
-            <Leaderboard />
+        <div className="app-container">
+            <header className="app-header">
+                <Header />
+            </header>
+            <main className="app-main">
+                <Leaderboard />
+            </main>
+           <footer className="app-footer">
+                <p>&copy; 2025 Leaderboard App. All rights reserved.</p>
+            </footer> 
         </div>
     );
 }
